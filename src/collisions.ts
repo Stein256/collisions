33b73dab8dbb0ee3dec8d7ab96cc086be013b272
Stein <izaya256@gmail.com>
2020-03-02 10:19:49 +0200

    Buggy collision detection

--- src/collisions.ts
+++ src/collisions.ts
@@ -1,70 +1,109 @@
-const trace = console.log;
+const trace = console.log
 
 export default class Collisions {
+    polygons: {}
     constructor() {
+        this.polygons = {}
     }
 
-    add() {
-
+    setCollidable(id, polygon) {
+        this.polygons[id] = { polygon}
     }
 
-    setPosition(x, y) {
-        
+    removeCollidable(id) {
+        delete this.polygons[id]
+    }
+
+    getProjection(id, polygon) {
+        for (let polygonID in this.polygons) {
+            if (id === polygonID) {
+                continue
+            }
+            let obstacle = this.polygons[polygonID].polygon
+            return doPolygonsIntersect(polygon, obstacle)
+        }
+        return {
+            x: 0,
+            y: 0,
+        }
     }
 }
 
 function doPolygonsIntersect(a, b) {
-    var polygons = [a, b];
-    var minA, maxA, projected, i, i1, j, minB, maxB;
+    let  polygons = [a, b]
+    let projection = {length: 0} // smallest projection vector
+    console.clear()
 
-    for (i = 0; i < polygons.length; i++) {
+    for (let i = 0; i < polygons.length; i++) {
 
         // for each polygon, look at each edge of the polygon, and determine if it separates
         // the two shapes
-        var polygon = polygons[i];
-        for (i1 = 0; i1 < polygon.length; i1++) {
+        const polygon = polygons[i]
+        for (let j = 0; j < polygon.length; j++) {
 
             // grab 2 vertices to create an edge
-            var i2 = (i1 + 1) % polygon.length;
-            var p1 = polygon[i1];
-            var p2 = polygon[i2];
+            const p1 = polygon[j]
+            const p2 = polygon[(j + 1) % polygon.length]
 
             // find the line perpendicular to this edge
-            var normal = { x: p2.y - p1.y, y: p1.x - p2.x };
+            const normal = normalize({ x: p2.y - p1.y, y: p1.x - p2.x })
+            // trace(normal.x, normal.y)
 
-            minA = maxA = undefined;
-            // for each vertex in the first shape, project it onto the line perpendicular to the edge
+            // for each vertex in both shapes project it onto the line perpendicular to the edge
             // and keep track of the min and max of these values
-            for (j = 0; j < a.length; j++) {
-                projected = normal.x * a[j].x + normal.y * a[j].y;
-                if (!minA || projected < minA) {
-                    minA = projected;
-                }
-                if (!maxA || projected > maxA) {
-                    maxA = projected;
-                }
-            }
+            const [minA, maxA] = minMax(a.map(p => dotProduct(p, normal)))
+            const [minB, maxB] = minMax(b.map(p => dotProduct(p, normal)))
 
-            // for each vertex in the second shape, project it onto the line perpendicular to the edge
-            // and keep track of the min and max of these values
-            minB = maxB = undefined;
-            for (j = 0; j < b.length; j++) {
-                projected = normal.x * b[j].x + normal.y * b[j].y;
-                if (!minB || projected < minB) {
-                    minB = projected;
-                }
-                if (!maxB || projected > maxB) {
-                    maxB = projected;
-                }
-            }
+            /*
+            const lengthA = maxA - minA
+            const lengthB = maxB - minB
+            const lengthAB = Math.abs(maxA - minB)
 
-            // if there is no overlap between the projects, the edge we are looking at separates the two
+            if (lengthAB > lengthA + lengthB) {
+                return { x: 0, y: 0, collided: false }
+            }
+            */
+
+            // if there is no overlap between the projections, the edge we are looking at separates the two
             // polygons, and we know there is no overlap
+            // trace(normal.x, normal.y, minA, maxA, minB, maxB)
+            trace(normal.x, normal.y, maxA - minA, maxB - minB, Math.abs(maxA - minB))
+            // trace(a[1].x, a[2].x)
+            trace(a[0].y, a[1].y)
             if (maxA < minB || maxB < minA) {
-                trace("polygons don't intersect!");
-                return false;
+                return { x: 0, y: 0, collided: false}
             }
         }
     }
-    return true;
+    return { x: 0, y: 0, collided: true}
+}
+
+function dotProduct(u: Vec2, v: Vec2): number {
+    return u.x * v.x + u.y + v.y
+}
+
+function normalize(v: Vec2): Vec2 {
+    let length = vectorLength(v)
+    return { x: v.x / length, y: v.y / length}
+}
+
+function vectorLength(v: Vec2): number {
+    return Math.sqrt(v.x ** 2 + v.y ** 2)
+}
+
+function minMax(arr: number[]): [number, number] {
+    let min = arr[0], max = arr[0]
+    for(let i = 1; i < arr.length; ++i) {
+        if (arr[i] < min) {
+            min = arr[i]
+        } else if (arr[i] > max) {
+            max = arr[i]
+        }
+    }
+    return [min, max]
+}
+
+interface Vec2 {
+    x: number,
+    y: number,
 }
--- src/scene.ts
+++ src/scene.ts
@@ -2,6 +2,9 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 import {DragControls} from 'three/examples/jsm/controls/DragControls'
 import * as THREE from 'three'
 
+import Collisions from './collisions'
+import { Mesh } from 'three'
+const trace = console.log
 
 export default class Scene {
     scene: THREE.Scene
@@ -9,53 +12,104 @@ export default class Scene {
     camera: THREE.PerspectiveCamera
     elem: HTMLElement
     dragControls: DragControls
+    collisions: Collisions
     constructor(elem: HTMLElement) {
         this.elem = elem
         this.renderer = new THREE.WebGLRenderer()
-        this.renderer.setSize(elem.offsetWidth, elem.offsetHeight);
+        this.renderer.setSize(elem.offsetWidth, elem.offsetHeight)
         elem.appendChild(this.renderer.domElement)
 
         this.scene = new THREE.Scene()
         this.scene.background = new THREE.Color(0xaaaaaa)
 
-        this.camera = new THREE.PerspectiveCamera(70, elem.offsetWidth / elem.offsetHeight, 0.1, 1000);
-        this.camera.position.set(0, 5, 20);
-        let orbitControls = new OrbitControls(this.camera, this.renderer.domElement);
-        orbitControls.addEventListener('change', () => this.render());
+        this.camera = new THREE.PerspectiveCamera(70, elem.offsetWidth / elem.offsetHeight, 0.1, 1000)
+        this.camera.position.set(0, 20, 0)
+        let orbitControls = new OrbitControls(this.camera, this.renderer.domElement)
+        orbitControls.addEventListener('change', () => this.render())
 
-        let gridHelper = new THREE.GridHelper(10, 10, 0xffffff, 0xcccccc);
-        this.scene.add(gridHelper);
+        let gridHelper = new THREE.GridHelper(10, 10, 0xffffff, 0xcccccc)
+        this.scene.add(gridHelper)
 
-        let boxGeometry = new THREE.BoxGeometry();
-        let material = new THREE.MeshBasicMaterial({ color: Math.random() * 0xffffff });
-        let box = new THREE.Mesh(boxGeometry, material);
+        let box1 = makeBox()
+        box1.position.x = 0
+        //box1.rotation.y = Math.PI / 2 * Math.random()
+        let box2 = makeBox()
+        box2.position.x = 10
+        //box2.rotation.y = Math.PI / 2 * Math.random()
 
-        this.scene.add(box)
+        this.collisions = new Collisions()
+        this.collisions.setCollidable(box1.uuid, meshToPoints(box1))
+        this.collisions.setCollidable(box2.uuid, meshToPoints(box2))
 
-        this.dragControls = new DragControls([box], this.camera, this.renderer.domElement)
+        this.scene.add(box1)
+        this.scene.add(box2)
+
+        let currentY = 0
+        this.dragControls = new DragControls([box1, box2], this.camera, this.renderer.domElement)
         this.dragControls.addEventListener('dragstart', e => {
             orbitControls.enabled = false
+            currentY = e.object.position.y
         })
 
+        let red = new THREE.MeshBasicMaterial({
+            color: 0xff0000
+        })
+        let green = new THREE.MeshBasicMaterial({
+            color: 0x00ff00
+        })
         this.dragControls.addEventListener('drag', e => {
+            e.object.position.y = currentY
+            //@ts-ignore
+            let { x, y, collided } = this.collisions.getProjection(e.object.uuid, meshToPoints(e.object))
+            e.object.position.x += x
+            e.object.position.z += y
+            if (collided) {
+                e.object.material = red
+            } else {
+                e.object.material = green
+            }
             this.render()
         })
 
         this.dragControls.addEventListener('dragend', e => {
             orbitControls.enabled = true
+            this.collisions.setCollidable(e.object.uuid, meshToPoints(e.object))
         })
 
         this.render()
     }
 
     render() {
-        this.renderer.render(this.scene, this.camera);
+        this.renderer.render(this.scene, this.camera)
     }
 
     resize() {
-        this.camera.aspect = this.elem.offsetWidth / window.innerHeight;
-        this.renderer.setSize(this.elem.offsetWidth, window.innerHeight);
-        this.camera.updateProjectionMatrix();
-        this.render();
+        this.camera.aspect = this.elem.offsetWidth / window.innerHeight
+        this.renderer.setSize(this.elem.offsetWidth, window.innerHeight)
+        this.camera.updateProjectionMatrix()
+        this.render()
     }
+}
+//check vertex order
+function meshToPoints(mesh: Mesh) {
+    //@ts-ignore
+    let p =  mesh.geometry.vertices
+        .filter(v => v.y > 0)
+        .map(v => ({
+            x: v.x + mesh.position.x,
+            y: v.z + mesh.position.z,
+        }));
+    // [p[1], p[2]] = [p[2], p[1]];
+    // [p[3], p[0]] = [p[0], p[3]];
+    return p
+}
+
+function makeBox() {
+    return new THREE.Mesh(
+        //new THREE.BoxGeometry(Math.random() * 5, 1, Math.random() * 5),
+        new THREE.BoxGeometry(2, 1, 4),
+        new THREE.MeshBasicMaterial({
+            color: Math.random() * 0xffffff
+        })
+    )
 }

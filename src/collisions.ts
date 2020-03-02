fb3eeade3dce751a8a699c879a116700313fd7b4
Stein <izaya256@gmail.com>
2020-03-02 10:25:35 +0200

    Propper collision detection

--- src/collisions.ts
+++ src/collisions.ts
@@ -1,85 +1,83 @@
 const trace = console.log
 
 export default class Collisions {
-    polygons: {}
+    polygons: PolygonDict
     constructor() {
         this.polygons = {}
     }
 
-    setCollidable(id, polygon) {
-        this.polygons[id] = { polygon}
+    setFromVertices(id: string, polygon: Vec2[], object = null) {
+        this.polygons[id] = { polygon, object }
     }
 
-    removeCollidable(id) {
+    remove(id: string) {
         delete this.polygons[id]
     }
 
-    getProjection(id, polygon) {
+    getProjection(id: string) {
+        const polygon = this.polygons[id].polygon
+        const total = {x: 0, y: 0, collided: false, objects: []} as CollisionResult
         for (let polygonID in this.polygons) {
             if (id === polygonID) {
                 continue
             }
             let obstacle = this.polygons[polygonID].polygon
-            return doPolygonsIntersect(polygon, obstacle)
+            const result = doPolygonsIntersect(polygon, obstacle)
+            total.x += result.x
+            total.y += result.y
+            total.collided = total.collided || result.collided
+            if(result.collided) {
+                total.objects.push(this.polygons[polygonID].object)
+            }
         }
-        return {
-            x: 0,
-            y: 0,
-        }
+        return total
     }
 }
 
-function doPolygonsIntersect(a, b) {
+function doPolygonsIntersect(a: Vec2[], b: Vec2[]) {
     let  polygons = [a, b]
-    let projection = {length: 0} // smallest projection vector
-    console.clear()
-
+    let projection = {length: Infinity} as {length: number, normal: Vec2} // smallest projection vector
+    
     for (let i = 0; i < polygons.length; i++) {
-
         // for each polygon, look at each edge of the polygon, and determine if it separates
         // the two shapes
         const polygon = polygons[i]
         for (let j = 0; j < polygon.length; j++) {
-
             // grab 2 vertices to create an edge
             const p1 = polygon[j]
             const p2 = polygon[(j + 1) % polygon.length]
 
             // find the line perpendicular to this edge
             const normal = normalize({ x: p2.y - p1.y, y: p1.x - p2.x })
-            // trace(normal.x, normal.y)
 
             // for each vertex in both shapes project it onto the line perpendicular to the edge
             // and keep track of the min and max of these values
             const [minA, maxA] = minMax(a.map(p => dotProduct(p, normal)))
             const [minB, maxB] = minMax(b.map(p => dotProduct(p, normal)))
 
-            /*
-            const lengthA = maxA - minA
-            const lengthB = maxB - minB
-            const lengthAB = Math.abs(maxA - minB)
-
-            if (lengthAB > lengthA + lengthB) {
-                return { x: 0, y: 0, collided: false }
-            }
-            */
-
-            // if there is no overlap between the projections, the edge we are looking at separates the two
-            // polygons, and we know there is no overlap
-            // trace(normal.x, normal.y, minA, maxA, minB, maxB)
-            trace(normal.x, normal.y, maxA - minA, maxB - minB, Math.abs(maxA - minB))
-            // trace(a[1].x, a[2].x)
-            trace(a[0].y, a[1].y)
-            if (maxA < minB || maxB < minA) {
+            const overlap = maxA - minA + maxB - minB - Math.abs(maxB - minA)
+            if (overlap <= 0) {
                 return { x: 0, y: 0, collided: false}
+            } else {
+                if (projection.length > overlap) {
+                    projection.length = overlap
+                    projection.normal = normal
+                }
             }
         }
     }
-    return { x: 0, y: 0, collided: true}
+    return {
+        x: projection.normal.x * projection.length,
+        y: projection.normal.y * projection.length, 
+        collided: true,
+    }
+}
+
+function addCollisionResults (results: CollisionResult) {
 }
 
 function dotProduct(u: Vec2, v: Vec2): number {
-    return u.x * v.x + u.y + v.y
+    return u.x * v.x + u.y * v.y
 }
 
 function normalize(v: Vec2): Vec2 {
@@ -106,4 +104,18 @@ function minMax(arr: number[]): [number, number] {
 interface Vec2 {
     x: number,
     y: number,
+}
+
+interface PolygonDict {
+    [key:string]: {
+        polygon: Vec2[]
+        object: any
+    }
+}
+
+interface CollisionResult {
+    x: number,
+    y: number,
+    collided: boolean
+    objects: any[]
 }
--- src/scene.ts
+++ src/scene.ts
@@ -3,7 +3,7 @@ import {DragControls} from 'three/examples/jsm/controls/DragControls'
 import * as THREE from 'three'
 
 import Collisions from './collisions'
-import { Mesh } from 'three'
+import { Mesh, Vec2, Geometry, BoxGeometry, Vector3, Object3D, SphereGeometry } from 'three'
 const trace = console.log
 
 export default class Scene {
@@ -30,23 +30,34 @@ export default class Scene {
         let gridHelper = new THREE.GridHelper(10, 10, 0xffffff, 0xcccccc)
         this.scene.add(gridHelper)
 
-        let box1 = makeBox()
-        box1.position.x = 0
-        //box1.rotation.y = Math.PI / 2 * Math.random()
-        let box2 = makeBox()
-        box2.position.x = 10
-        //box2.rotation.y = Math.PI / 2 * Math.random()
+        let box1 = makeBox(new Vector3(-2, 0, 0))
+        let box2 = makeBox(new Vector3(0, 0, 0))
+        let box3 = makeBox(new Vector3(2, 0, 0))
 
-        this.collisions = new Collisions()
-        this.collisions.setCollidable(box1.uuid, meshToPoints(box1))
-        this.collisions.setCollidable(box2.uuid, meshToPoints(box2))
+        // box1.rotation.y = Math.PI * 2 * Math.random()
+        // box2.rotation.y = Math.PI * 2 * Math.random()
+        // box3.rotation.y = Math.PI * 2 * Math.random()
+
+        box1.rotation.y = Math.PI / 10
+        box2.rotation.y = 0
+        box3.rotation.y = -Math.PI / 10
 
         this.scene.add(box1)
         this.scene.add(box2)
+        this.scene.add(box3)
+
+        box1.updateMatrix()
+        box2.updateMatrix()
+        box3.updateMatrix()
+
+        this.collisions = new Collisions()
+        this.collisions.setFromVertices(box1.uuid, meshToPoints(box1))
+        this.collisions.setFromVertices(box2.uuid, meshToPoints(box2))
+        this.collisions.setFromVertices(box3.uuid, meshToPoints(box3))
 
         let currentY = 0
-        this.dragControls = new DragControls([box1, box2], this.camera, this.renderer.domElement)
-        this.dragControls.addEventListener('dragstart', e => {
+        this.dragControls = new DragControls([box1, box2, box3], this.camera, this.renderer.domElement)
+        this.dragControls.addEventListener('dragstart', (e: DragEvent) => {
             orbitControls.enabled = false
             currentY = e.object.position.y
         })
@@ -57,12 +68,12 @@ export default class Scene {
         let green = new THREE.MeshBasicMaterial({
             color: 0x00ff00
         })
-        this.dragControls.addEventListener('drag', e => {
+        this.dragControls.addEventListener('drag', (e: DragEvent) => {
             e.object.position.y = currentY
-            //@ts-ignore
-            let { x, y, collided } = this.collisions.getProjection(e.object.uuid, meshToPoints(e.object))
-            e.object.position.x += x
-            e.object.position.z += y
+            this.collisions.setFromVertices(e.object.uuid, meshToPoints(e.object))
+            let { x, y, collided } = this.collisions.getProjection(e.object.uuid)
+            e.object.position.x -= x
+            e.object.position.z -= y
             if (collided) {
                 e.object.material = red
             } else {
@@ -71,11 +82,25 @@ export default class Scene {
             this.render()
         })
 
-        this.dragControls.addEventListener('dragend', e => {
+        this.dragControls.addEventListener('dragend', (e: DragEvent) => {
             orbitControls.enabled = true
-            this.collisions.setCollidable(e.object.uuid, meshToPoints(e.object))
+            let objects, calls = 0;
+            do {
+                ++calls
+                this.collisions.setFromVertices(e.object.uuid, meshToPoints(e.object))
+                let res = this.collisions.getProjection(e.object.uuid)
+                e.object.position.x -= res.x
+                e.object.position.z -= res.y
+                objects = res.objects
+                e.object.updateMatrix()
+            } while (objects.length && calls < 1000)
+            trace('calls', calls)
+            if (calls < 100) {
+                e.object.material = green
+            }
+            this.collisions.setFromVertices(e.object.uuid, meshToPoints(e.object))
+            this.render()
         })
-
         this.render()
     }
 
@@ -90,26 +115,41 @@ export default class Scene {
         this.render()
     }
 }
-//check vertex order
-function meshToPoints(mesh: Mesh) {
-    //@ts-ignore
-    let p =  mesh.geometry.vertices
+
+function meshToPoints(mesh: Mesh & {geometry: Geometry}) {
+    return mesh.geometry.vertices
         .filter(v => v.y > 0)
+        .map(v => v.clone().applyMatrix4(mesh.matrix))
         .map(v => ({
-            x: v.x + mesh.position.x,
-            y: v.z + mesh.position.z,
-        }));
-    // [p[1], p[2]] = [p[2], p[1]];
-    // [p[3], p[0]] = [p[0], p[3]];
-    return p
+            x: v.x,
+            y: v.z,
+        } as Vec2))
 }
 
-function makeBox() {
-    return new THREE.Mesh(
+function makeBox(position: Vector3) {
+    const box = new THREE.Mesh(
         //new THREE.BoxGeometry(Math.random() * 5, 1, Math.random() * 5),
-        new THREE.BoxGeometry(2, 1, 4),
+        new THREE.BoxGeometry(2, 0.000001, 4),
         new THREE.MeshBasicMaterial({
             color: Math.random() * 0xffffff
         })
-    )
+    ) as Mesh & {geometry: BoxGeometry}
+    box.position.copy(position)
+    return box
+}
+
+function makeSphere(position: Vector3) {
+    const sphere = new THREE.Mesh(
+        //new THREE.BoxGeometry(Math.random() * 5, 1, Math.random() * 5),
+        new THREE.SphereGeometry(0.1, 1, 1),
+        new THREE.MeshBasicMaterial({
+            color: 0xff0000
+        })
+    ) as Mesh & {geometry: SphereGeometry}
+    sphere.position.copy(position)
+    return sphere
+}
+
+interface DragEvent extends THREE.Event {
+    object: Mesh & { geometry: Geometry }
 }
